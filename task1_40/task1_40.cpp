f8020ce74dd8bf2ee63bc7fc7719444ca441cb93
onixuniverse <48064744+onixuniverse@users.noreply.github.com>
2023-02-25 14:07:24 +0500

    Create task1_40.cpp

---
+++ task1_40/task1_40.cpp
@@ -0,0 +1,12 @@
+#include "Task140.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	task140::Task140 form;
+	Application::Run(% form);
+}

2a92165493d93ee42fba0a433f08e18f4842d244
onixuniverse <48064744+onixuniverse@users.noreply.github.com>
2023-03-24 00:28:57 +0500

    Create task4_25.cpp

---
+++ task4_25/task4_25.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+struct RationalNumber {
+    int numerator;
+    int determinator;
+} r;
+
+
+vector<RationalNumber> createFillArray(int count) {
+    vector<RationalNumber> arr;
+
+    for (int i = 0; i <= count; i++) {
+        RationalNumber rn;
+        rn.numerator = rand() % 11;
+        rn.determinator = rand() % 11 + 1;
+
+        arr.push_back(rn);
+    }
+
+    return arr;
+}
+
+tuple<bool, RationalNumber> findEqual(vector<RationalNumber> arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = 0; j < arr.size(); j++) {
+            if (i != j) {
+                if ((arr[i].numerator / arr[i].determinator) == (arr[j].numerator / arr[j].determinator)) {
+                    return { true, arr[i] };
+                }
+            }
+        }
+    }
+    return { false, r };
+}
+
+RationalNumber findMaxRationalNumber(vector<RationalNumber> arr) {
+    RationalNumber maxNumber;
+    maxNumber.numerator = 0;
+    maxNumber.determinator = 1;
+
+    for (int i = 0; i < arr.size(); i++) {
+        if ((arr[i].numerator / arr[i].determinator) > (maxNumber.numerator / maxNumber.determinator))
+            maxNumber = arr[i];
+    }
+    return maxNumber;
+}
+
+int main()
+{
+    int count = 50;
+    bool flagOne;
+    RationalNumber resultOne, resultTwo;
+
+    srand(time(0));
+    vector<RationalNumber> arrRationalNumbers;
+    arrRationalNumbers = createFillArray(count);
+
+    tie(flagOne, resultOne) = findEqual(arrRationalNumbers);
+    resultTwo = findMaxRationalNumber(arrRationalNumbers);
+    
+
+    for (int i = 0; i < arrRationalNumbers.size(); i++) {
+        cout << arrRationalNumbers[i].numerator << "/" << arrRationalNumbers[i].determinator << ", ";
+    }
+    cout << endl;
+
+    if (flagOne)
+        printf("Equal: %d/%d == %d/%d\n", resultOne.numerator, resultOne.determinator, resultOne.numerator, resultOne.determinator);
+    else
+        cout << "There are no equal values" << endl;
+
+    printf("Max: %d/%d\n", resultTwo.numerator, resultTwo.determinator);
+}

aee4c8de39f757b7de8b6dd0dc30b7fd9021641d
onixuniverse <48064744+onixuniverse@users.noreply.github.com>
2023-04-12 15:30:57 +0500

    Create Templates.h

---
+++ task6_25/Templates.h
@@ -0,0 +1,11 @@
+#ifndef TEMPLATES__H
+#define TEMPLATES__H
+#include <vector>
+#include <string>
+
+
+inline std::vector<std::string> words;
+
+void readFile(System::String^ inputFilePathManaged);
+
+#endif

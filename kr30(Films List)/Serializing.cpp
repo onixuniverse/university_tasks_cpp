a54d2a70547f91f01da0eec9eac3c21f1fc42027
onixuniverse <48064744+onixuniverse@users.noreply.github.com>
2023-06-01 22:11:55 +0500

    Update Serializing.cpp

--- kr30(Films List)/Serializing.cpp
+++ kr30(Films List)/Serializing.cpp
@@ -2,21 +2,16 @@
 
 using namespace rapidxml;
 
-// Функция для сериализации списка фильмов в XML
 void serializeFilmsToXml(const std::vector<Film>& Films, const std::string& filename) {
     xml_document<> doc;
 
-    // Создание корневого элемента "Films"
     xml_node<>* rootNode = doc.allocate_node(node_element, "films");
     doc.append_node(rootNode);
 
-    // Сериализация каждого фильма
     for (const Film& Film : Films) {
-        // Создание элемента "Film"
         xml_node<>* FilmNode = doc.allocate_node(node_element, "film");
         rootNode->append_node(FilmNode);
 
-        // Создание и заполнение элементов фильма
         xml_node<>* idNode = doc.allocate_node(node_element, "id", Film.id.c_str());
         FilmNode->append_node(idNode);
 
@@ -29,7 +24,6 @@ void serializeFilmsToXml(const std::vector<Film>& Films, const std::string& file
         xml_node<>* durationNode = doc.allocate_node(node_element, "duration", Film.duration.c_str());
         FilmNode->append_node(durationNode);
 
-        // Создание и заполнение элементов списков
         for (const std::string& director : Film.directors) {
             xml_node<>* directorNode = doc.allocate_node(node_element, "director", director.c_str());
             FilmNode->append_node(directorNode);
@@ -46,7 +40,6 @@ void serializeFilmsToXml(const std::vector<Film>& Films, const std::string& file
         }
     }
 
-    // Сохранение XML в файл
     std::ofstream file(filename);
     file << doc;
     file.close();
